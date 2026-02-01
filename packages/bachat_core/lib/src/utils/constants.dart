624052cacf538a1f1db2bed0327e7406eca47363
Rohit Nag <rohitnag@Rohits-Mac-mini.local>
2026-02-01 17:36:15 +0530

    Production: Updated backend URL to Render and finalized production settings.

--- packages/bachat_core/lib/src/utils/constants.dart
+++ packages/bachat_core/lib/src/utils/constants.dart
@@ -1,3 +1,3 @@
 class AppConstants {
-  static const String baseUrl = 'http://192.168.1.67:5001/api'; // Use machine IP for physical device
+  static const String baseUrl = 'https://bachat-g-backend.onrender.com/api';
 }
